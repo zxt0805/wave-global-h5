7d69dd78420e6d52c7ce1eaa3a3ed36093554c84
weixuefeng <weixuefeng@diynova.com>
2022-09-29 14:20:13 +0800

    fix: call params

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-09-29 14:09:47
+ * @LastEditTime: 2022-09-29 14:19:48
  * @FilePath: /wave-chinese-website/src/pages/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -94,7 +94,7 @@ function postMessage(params, callback) {
   if (window && window.flutter_inappwebview) {
     console.log("send info android");
     // @ts-ignore
-    window.flutter_inappwebview.callHandler('postMessage', JSON.stringify(params), callback)
+    window.flutter_inappwebview.callHandler(JSON.stringify(params), callback)
     // @ts-ignore
   } else if(window && window.webkit){
     console.log("send info ios");
