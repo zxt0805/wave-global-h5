c4c609dc02bc6041a0f951e6585d991cf5a06a6f
weixuefeng <weixuefeng@diynova.com>
2022-09-29 14:10:00 +0800

    feat: add console

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-09-29 13:19:49
+ * @LastEditTime: 2022-09-29 14:09:47
  * @FilePath: /wave-chinese-website/src/pages/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -92,10 +92,12 @@ function requestRoute() {
 function postMessage(params, callback) {
   // @ts-ignore
   if (window && window.flutter_inappwebview) {
+    console.log("send info android");
     // @ts-ignore
     window.flutter_inappwebview.callHandler('postMessage', JSON.stringify(params), callback)
     // @ts-ignore
   } else if(window && window.webkit){
+    console.log("send info ios");
     // @ts-ignore, add ios callback
     window.webkit.messageHandlers[handler].postMessage(params)
   } else {
