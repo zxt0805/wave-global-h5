dd6f14dbd544a8da613046dfbed03ba4c1fffb20
zhuxiaotong <zhuxiaotong@diynova.com>
2022-09-30 15:59:18 +0800

    feat: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-09-30 15:55:58
+ * @LastEditTime: 2022-09-30 15:58:48
  * @FilePath: /wave-chinese-website/src/pages/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -103,7 +103,7 @@ function postMessage(params, callback) {
 
 function Main() {
   const [saleStatus, setSaleStatus] = useState('saling')
-  const [countDown, setCountDown] = useState(0)
+  const [countDown, setCountDown] = useState(1000000)
   // var countDown = 0
 
   useEffect(() => {
