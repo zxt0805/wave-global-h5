3ce1f8b0d589da0c48e2899f723e5b987cbd43bf
weixuefeng <weixuefeng@diynova.com>
2022-10-08 20:30:08 +0800

    refactor: code

--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
- * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-08 20:09:29
+ * @LastEditors: weixuefeng weixuefeng@diynova.com
+ * @LastEditTime: 2022-10-08 20:28:01
  * @FilePath: /wave-chinese-website/src/pages/collection/[id].tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,119 +16,22 @@ import { PageModel } from 'model/navModel'
 import { useEffect, useState } from 'react'
 
 import { postRequest } from '../../services/getAxios'
+import { useRouter } from 'next/router'
 
 export default Home
 
 function Home() {
   let pageModel = new PageModel('北京压缩矩阵科技有限公司', 'WAVE', '')
-
   return <>{NormalLayout(Main(), pageModel)}</>
 }
 
-let params = {
-  name: 'requestUserInfo',
-  data: {},
-}
-postMessage(params, function (data) {
-  if (data != null) {
-    console.log(JSON.stringify(data))
-  }
-})
-
-function requestAddCalendar() {
-  console.log('call requestAddCalendar')
-
-  let params = {
-    name: 'requestCalendar',
-    data: {
-      collection_id: '1',
-      title: 'Wave平台EVT【胜利】开售，快去看看吧！',
-      description: 'Wave平台EVT【胜利】开售，快去看看吧！',
-      start_time: '1665331200',
-      end_time: '16653312300',
-    },
-  }
-  postMessage(params, function (data) {
-    if (data != null) {
-      console.log(JSON.stringify(data))
-    }
-  })
-}
-
-function checkCalendar() {
-  let params = {
-    name: 'checkCalendar',
-    data: {
-      collection_id: '1',
-      title: 'Wave平台EVT【胜利】开售，快去看看吧！',
-      description: 'Wave平台EVT【胜利】开售，快去看看吧！',
-      start_time: '1665331200',
-      end_time: '16653312300',
-    },
-  }
-  postMessage(params, function (data) {
-    if (data != null) {
-      console.log(JSON.stringify(data))
-    }
-  })
-}
-
-function requestPayOrder() {
-  console.log('call requestPayOrder')
-
-  let params = {
-    name: 'requestPayOrder',
-    data: {
-      collection_id: 1,
-      number: 5,
-      price: '1233434',
-      to_address: 'NEW182XXX',
-    },
-  }
-  postMessage(params, function (data) {
-    if (data != null) {
-      console.log(JSON.stringify(data))
-    }
-  })
-}
-
-function requestRoute() {
-  console.log('call requestRoute')
-
-  let params = {
-    name: 'requestRoute',
-    data: {
-      path: '/detail/',
-      params: {},
-    },
-  }
-  postMessage(params, function (data) {
-    if (data != null) {
-      console.log(JSON.stringify(data))
-    }
-  })
-}
-
-function postMessage(params, callback) {
-  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-    // @ts-ignore
-    if (window && window.flutter_inappwebview) {
-      console.log('send info android')
-      // @ts-ignore
-      window.flutter_inappwebview.callHandler(JSON.stringify(params), callback)
-      // @ts-ignore
-    } else if (window && window.webkit) {
-      console.log('send info ios')
-      // @ts-ignore, add ios callback
-      window.webkit.messageHandlers[handler].postMessage(params)
-    } else {
-      console.log(JSON.stringify(params))
-    }
-  }
-}
 
 function Main() {
   const collectionUrl = '/api/collection'
+  const router = useRouter()
+  const { id } = router.query
+  console.log("collection id " + id);
+  
   useEffect(() => {
     let params = {
       collection_id: '4',
@@ -149,10 +52,8 @@ function Main() {
     setSaleStatus('saling')
     requestUserInfo()
     checkCalendar()
-    // setTimeout(() => {
-    //   setCountDown(1000000)
-    // }, 2000)
-  }, [])
+  }, [0])
+
   return (
     <div className="index-wrap">
       <HeadImg></HeadImg>
@@ -168,10 +69,6 @@ function Main() {
         <img src="/assets/image/icon_arrow.png" alt="" />
       </div>
       {isLogin ? <>login</> : <>not login</>}
-      {/* <button className={"button"} onClick={() => requestUserInfo()}>请求获取用户信息</button>
-      <button className={"button"} onClick={() => requestAddCalander()}>请求添加日历</button>
-      <button className={"button"} onClick={() => requestPayOrder()}>请求支付订单</button>
-      <button className={"button"} onClick={() => requestRoute()}>请求路由</button> */}
       <FixBottom
         saleStatus={saleStatus}
         addToCalendar={() => requestAddCalendar()}
@@ -179,22 +76,10 @@ function Main() {
         gotoTrade={() => requestRoute()}
       />
     </div>
-    /* <About /> */
-    /* <Features />
-    <>
-      <Banner />
-      
-      {/* <About /> */
-    /* <Features />
-      <Roadmap />
-      <RoadmapMobile />
-      <Faq />
-      <Down /> */
   )
 
   function requestUserInfo() {
     console.log('call requestUserInfo')
-
     let params = {
       name: 'requestUserInfo',
       data: {},
