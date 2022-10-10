e05e37ee3662fdc5d8c4c5d91a50aa20f9b780d9
zhuxiaotong <zhuxiaotong@diynova.com>
2022-10-10 17:40:12 +0800

    feature: calendar status

--- src/components/collection/fixedBottom.tsx
+++ src/components/collection/fixedBottom.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 16:09:48
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-10 17:37:09
+ * @LastEditTime: 2022-10-10 17:39:57
  * @FilePath: /wave-global-h5/src/components/index/fixBottom.tsx
  * @LastEditors: weixuefeng weixuefeng@diynova.com
  * @LastEditTime: 2022-10-09 17:02:42
@@ -16,26 +16,26 @@ import { t } from 'i18next'
 export default fixedBottom
 
 function fixedBottom(props) {
-  const { addToCalendar, payOrder, gotoTrade, gotoAssets, collectionInfo } = props
+  const { addToCalendar, payOrder, gotoTrade, gotoAssets, collectionInfo, hasAddCalendar } = props
   function saleStatusChange() {
     if (collectionInfo.sell_status == 0) {
-      // TODO: 加入日历状态判断按钮展示
-      // 未加
-      return (
-        <div className="fixBottom">
-          <div className="statusInfo">{collectionInfo.sell_price}NEW</div>
-          <div className={collectionInfo.sell_start_time - collectionInfo.system_time < 600 ? 'button button-disable' : 'button'} onClick={collectionInfo.sell_start_time - collectionInfo.system_time < 600 ? (() => {}) : addToCalendar}>
-            {t('ADDTOCALENDAR')}
+      if(hasAddCalendar){
+        return (
+          <div className="fixBottom">
+            <div className="statusInfo">{collectionInfo.sell_price}NEW</div>
+            <div className="button button-disable">{t('ADDEDTOCALENDAR')}</div>
           </div>
-        </div>
-      )
-      // 已加
-      return (
-        <div className="fixBottom">
-          <div className="statusInfo">{collectionInfo.sell_price}NEW</div>
-          <div className="button button-disable">{t('ADDEDTOCALENDAR')}</div>
-        </div>
-      )
+        )
+      } else {
+        return (
+          <div className="fixBottom">
+            <div className="statusInfo">{collectionInfo.sell_price}NEW</div>
+            <div className={collectionInfo.sell_start_time - collectionInfo.system_time < 600 ? 'button button-disable' : 'button'} onClick={collectionInfo.sell_start_time - collectionInfo.system_time < 600 ? (() => {}) : addToCalendar}>
+              {t('ADDTOCALENDAR')}
+            </div>
+          </div>
+        )
+      }
     } else if(collectionInfo.sell_status == 1){
       return (
         <div className="fixBottom">
--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-10 17:08:01
+ * @LastEditTime: 2022-10-10 17:39:19
  * @FilePath: /wave-chinese-website/src/pages/collection/[id].tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -260,6 +260,7 @@ function Main() {
           <img src="/assets/image/icon_arrow.png" alt="" />
         </a>
         <FixBottom
+          hasAddCalendar={hasAddCalendar}
           collectionInfo={collectionInfo}
           addToCalendar={() => requestAddCalendar()}
           payOrder={() => requestPayOrder()}
