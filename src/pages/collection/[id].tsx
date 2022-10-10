18999ae9a4258baa2379ee3d86a0fdaff01c1fca
weixuefeng <weixuefeng@diynova.com>
2022-10-10 11:45:29 +0800

    fix: update effect config

--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-09 22:25:44
+ * @LastEditTime: 2022-10-10 11:43:08
  * @FilePath: /wave-chinese-website/src/pages/collection/[id].tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -43,12 +43,13 @@ function Main() {
   const collectionUrl = '/api/collection'
 
   useEffect(() => {
+    console.log("init");
     requestLanguage()
     requestUserInfo()
     if (id != undefined) {
       fetchCollectionInfo()
     }
-  }, [isLogin, id])
+  }, [id])
 
 
   function fetchCollectionInfo() {
