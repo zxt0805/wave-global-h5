2feb105526c694887f841d378a3f9b510b8d674d
zhuxiaotong <zhuxiaotong@diynova.com>
2022-10-11 14:48:56 +0800

    fix: title

--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-11 14:07:53
+ * @LastEditTime: 2022-10-11 14:48:44
  * @FilePath: /wave-chinese-website/src/pages/collection/[id].tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ import { IS_DEBUG } from 'constants/settings'
 export default Home
 
 function Home() {
-  let pageModel = new PageModel('WAVE', 'WAVE', '')
+  let pageModel = new PageModel('COLLECTION', 'WAVE', '')
   return <>{NormalLayout(Main(), pageModel)}</>
 }
 
