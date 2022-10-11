f5bd61762c93ac8ba903d6e1b2b23c55e5e2b240
liukeke <liukeke@diynova.com>
2022-10-11 15:45:41 +0800

    fix: share

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-09-21 10:43:33
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-09 20:12:05
+ * @LastEditTime: 2022-10-11 15:42:15
  * @FilePath: /wave-chinese-website/src/pages/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,5 +18,8 @@ function Home() {
 }
 
 function Main() {
-  return <>Hello Wave</>
+  return <>
+    <div className='hidden'><img alt='logo' src='/assets/image/logo.png' /></div>
+    Hello Wave
+  </>
 }
