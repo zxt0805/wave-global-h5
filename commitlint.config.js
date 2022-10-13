4a0df1389d511d1bb1509f5fabaa502809e0be48
liukeke <liukeke@diynova.com>
2022-10-13 17:03:57 +0800

    feat: commit
    add commit

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: liukeke liukeke@diynova.com
+ * @Date: 2022-10-13 16:56:51
+ * @LastEditors: liukeke liukeke@diynova.com
+ * @LastEditTime: 2022-10-13 16:57:00
+ * @FilePath: /wave-global-h5/commitlint.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional']
+}
