c6df5202c8d5cf24a525ebdba37165782514a01e
zhuxiaotong <zhuxiaotong@diynova.com>
2022-10-20 16:08:39 +0800

    fix: setInterval

--- src/components/collection/headImg.tsx
+++ src/components/collection/headImg.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
- * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-14 15:48:20
+ * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
+ * @LastEditTime: 2022-10-20 16:07:19
  * @FilePath: /wave-global-h5/src/components/collection/headImg.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,14 +27,22 @@ function HeadImg(props) {
         if (timer) {
           clearInterval(timer)
         }
-        countDown()
+        countDown(remainTime)
       }
+    }
+    return () => {
+      clearInterval(timer)
     }
   }, [])
 
-  function countDown() {
+  function countDown(startTime) {
+    let countDownTime = startTime
     timer = setInterval(() => {
-      setRemainSecond(remainSecond - 1)
+      if(countDownTime > 1){
+        setRemainSecond(-- countDownTime)
+      } else {
+        clearInterval(timer)
+      }
     }, 1000)
   }
 
