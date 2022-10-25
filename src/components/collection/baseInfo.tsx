7cb7fe4115a9d75405cab91470c06ea96287177e
zhuxiaotong <zhuxiaotong@diynova.com>
2022-10-25 20:03:10 +0800

    fix: remain number

--- src/components/collection/baseInfo.tsx
+++ src/components/collection/baseInfo.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-09 16:42:17
+ * @LastEditTime: 2022-10-25 20:02:07
  * @FilePath: /wave-chinese-website/src/components/index/baseInfo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,7 +22,7 @@ function BaseInfo(props) {
       <div className="nums">
         <div className="nums-item">
           <span className="tip">{t('REMAINING')}</span>
-          <span className="number">{collectionInfo.box_total - collectionInfo.sold_quantity}</span>
+          <span className="number">{collectionInfo.box_total - collectionInfo.sold_quantity > 0 ? collectionInfo.box_total - collectionInfo.sold_quantity : 0}</span>
         </div>
         <div className="nums-item">
           <span className="tip">{t('TOTALSUPPLY')}</span>
