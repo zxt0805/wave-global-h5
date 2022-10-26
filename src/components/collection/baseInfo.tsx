3659c0c17632e76965d3ae6a8ea51ebecc74ac8a
weixuefeng <weixuefeng@diynova.com>
2022-10-26 11:27:17 +0800

    fix: 180

--- src/components/collection/baseInfo.tsx
+++ src/components/collection/baseInfo.tsx
@@ -1,9 +1,9 @@
 /*
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
- * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-09 16:42:17
- * @FilePath: /wave-chinese-website/src/components/index/baseInfo.tsx
+ * @LastEditors: weixuefeng weixuefeng@diynova.com
+ * @LastEditTime: 2022-10-26 11:26:31
+ * @FilePath: /wave-global-h5/src/components/collection/baseInfo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect, useState } from 'react'
@@ -25,7 +25,7 @@ function BaseInfo(props) {
           <span className="number">{collectionInfo.box_total - collectionInfo.sold_quantity}</span>
         </div>
         <div className="nums-item">
-          <span className="tip">{t('TOTALSUPPLY')}</span>
+          <span className="tip">{t('CURRENT_RELEASE')}</span>
           <span className="number">{collectionInfo.box_total}</span>
         </div>
       </div>
