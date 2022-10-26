2f302de2c125a3b375609d38a372057f63758744
zhuxiaotong <zhuxiaotong@diynova.com>
2022-10-26 16:36:13 +0800

    update: share-test

--- src/components/Layout/normalLayout.tsx
+++ src/components/Layout/normalLayout.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 11:46:48
  * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-26 15:50:40
+ * @LastEditTime: 2022-10-26 16:36:07
  * @FilePath: /wave-global-h5/src/components/Layout/normalLayout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ export default function NormalLayout(children: React.ReactNode, pageModel: PageM
         <meta property="og:type" content="website" />
         <meta property="og:title" content={pageModel?.title} />
         <meta property="og:description" content={pageModel?.description || 'WAVE'} />
-        <meta property="og:image" content={pageModel?.image || "https://app.waveuniverse.org/assets/image/banner-h5-new.png"} />
+        <meta property="og:image" content={pageModel?.image} />
         {/* <meta property="og:url" content="https://app.waveuniverse.org/" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
